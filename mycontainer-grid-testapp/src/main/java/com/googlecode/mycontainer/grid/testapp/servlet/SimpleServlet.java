31caf524a1c37e674ecb0a44475466a77ca1196d
Fernando Ultremare <feroult@gmail.com>
2012-10-25 21:59:11 -0200

    start

---
+++ mycontainer-grid-testapp/src/main/java/com/googlecode/mycontainer/grid/testapp/servlet/SimpleServlet.java
@@ -0,0 +1,22 @@
+package com.googlecode.mycontainer.grid.testapp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -3388076540989725515L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		resp.getWriter().println("simple servlet");
+	}
+
+
+}
