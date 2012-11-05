a38102c6f52c355312b756a12a908c45d36931f5
Fernando Ultremare <feroult@gmail.com>
2012-11-04 23:35:33 -0200

    .

---
+++ mycontainer-grid/src/main/java/com/googlecode/mycontainer/grid/server/setup/HSQLDataSourceSetup.java
@@ -0,0 +1,15 @@
+package com.googlecode.mycontainer.grid.server.setup;
+
+import com.googlecode.mycontainer.datasource.DataSourceDeployer;
+
+public class HSQLDataSourceSetup extends DataSourceSetup {
+
+	@Override
+	public void set(DataSourceDeployer deployer, String partition) {
+		deployer.setName(getName());
+		deployer.setDriver("org.hsqldb.jdbcDriver");
+		deployer.setUrl("jdbc:hsqldb:mem:" + partition);
+		deployer.setUser("sa");
+	}
+
+}
---
+++ mycontainer-grid/src/main/java/com/googlecode/mycontainer/grid/server/setup/HSQLJPASetup.java
@@ -0,0 +1,16 @@
+package com.googlecode.mycontainer.grid.server.setup;
+
+import java.util.Properties;
+
+import com.googlecode.mycontainer.jpa.JPAInfoBuilder;
+
+public class HSQLJPASetup extends JPASetup {
+
+	protected void setupCustomProperties(JPAInfoBuilder info) {
+		Properties props = info.getProperties();
+		props.setProperty("hibernate.dialect",
+				"org.hibernate.dialect.HSQLDialect");
+		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		props.setProperty("hibernate.show_sql", "true");
+	}
+}
